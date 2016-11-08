4ef8eb195ae9471b1973f414b2c5b79a5439a554
Volker E <volker.e@wikimedia.org>
2016-11-08 05:05:50 -0800

    Initial import

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+/*!
+ * Grunt file
+ */
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		// Lint – Styles
+		stylelint: {
+			src: [
+				'css/*.dev.css',
+				//'css/partials/**',
+				'!node_modules/**'
+			]
+		},
+
+		// Postprocessing Styles
+		postcss: {
+			options: {
+				processors: [
+					require( 'postcss-import' )( {
+						from: "css/wmui-styleguide.dev.css"
+					} ),
+					// require( 'postcss-cssnext' )(),
+					require( 'postcss-custom-properties' ),
+					require( 'autoprefixer' )( {
+						browsers: [
+							"Android >= 2.3",
+							"Chrome >= 10",
+							"Edge >= 12",
+							"Firefox >= 3.6",
+							"IE >= 8",
+							"IE_mob 11",
+							"iOS >= 5.1",
+							"Opera >= 12.5",
+							"Safari >= 5.1"
+						]
+					} )
+				]
+			},
+			dist: {
+				files: {
+					'css/wmui-styleguide.css': 'css/wmui-styleguide.dev.css'
+				}
+			}
+		},
+
+		cssmin: {
+			options: {
+				shorthandCompacting: true,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+				  'css/wmui-styleguide.min.css': 'css/wmui-styleguide.css'
+				}
+			}
+		},
+
+		// Development
+		watch: {
+			files: [
+				'**/*.css',
+				'.{stylelintrc}'
+			],
+			tasks: 'default'
+		}
+
+	} );
+
+	grunt.registerTask( 'lint', [ 'stylelint' ] );
+	grunt.registerTask( 'default', 'lint', 'postcss', 'cssmin' );
+};
