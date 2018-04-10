0490f7c7aaf3e8f0b3cd5d0f2a8f4a708edaa55e
Volker E <volker.e@wikimedia.org>
2018-04-10 02:50:52 -0700

    styles: Change sans-serif font stack to system fonts Changing sans-serif font stack to new system font preferred one. Also removing all Lato web font references and files and moving fonts-loaded.css contents into main CSS file. Bug: [T188799](https://phabricator.wikimedia.org/T188799)

--- js/fonts-loader.js
+++ js/fonts-loader.js
@@ -9,11 +9,9 @@ if ( document.head && 'Promise' in window ) {
 		script.src = './js/vendor/fontfaceobserver/fontfaceobserver.standalone.js';
 
 		script.onload = function () {
-			var sansSerif = new FontFaceObserver( 'Lato' );
 			var serif = new FontFaceObserver( 'Charter' );
 
 			Promise.all( [
-				sansSerif.load(),
 				serif.load()
 			] ).then( function () {
 				html.classList.add( 'fonts-loaded' );
