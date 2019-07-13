f5a3e48dcc4171cb43d36fea7653b1dd5021a6fb
Volker E <Volker-E@users.noreply.github.com>
2019-07-13 21:20:05 +0200

    build: Move 'autoprefixer' configuration to package.json (#213) This is nowadays the recommended location for it, also slightly updating supported browsers and building CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,7 @@ module.exports = function ( grunt ) {
 			require( 'postcss-custom-properties' )( {
 				preserve: false
 			} ),
-			require( 'autoprefixer' )( {
-				browsers: [
-					'Android >= 2.3',
-					'Chrome >= 10',
-					'Edge >= 12',
-					'Firefox >= 3.6',
-					'IE >= 8',
-					'IE_mob 11',
-					'iOS >= 5.1',
-					'Opera >= 12.5',
-					'Safari >= 5.1'
-				]
-			} )
+			require( 'autoprefixer' )()
 		],
 		// With minifier
 		postCssProcessorsMin = postCssProcessorsDev.concat( [ require( 'cssnano' )() ] ),
