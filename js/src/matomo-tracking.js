88b6b6197d9a40bd0616229f240f8fbde2b79c01
Volker E <volker.e@wikimedia.org>
2019-12-19 15:41:28 -0800

    Fix Matomo external analytics script, second attempt
    Define as implicit globals.

--- js/src/matomo-tracking.js
+++ js/src/matomo-tracking.js
@@ -3,31 +3,30 @@
  * @see https://matomo.org/blog/2017/04/different-ways-embedding-piwik-tracking-code-faster-website-performance/
  * @see https://matomo.org/faq/general/faq_20904/
  */
-( function () {
-	/* eslint-disable-next-line no-underscore-dangle */
-	var _paq = window._paq || [];
-	_paq.push( [ 'setDomains', [ '*.design.wikimedia.org.' ] ] );
-	_paq.push( [ 'trackPageView' ] );
-	_paq.push( [ 'enableLinkTracking' ] );
+/* eslint-disable-next-line no-underscore-dangle, no-implicit-globals */
+var _paq = window._paq || [];
+_paq.push( [ 'setDomains', [ '*.design.wikimedia.org.' ] ] );
+_paq.push( [ 'trackPageView' ] );
+_paq.push( [ 'enableLinkTracking' ] );
 
-	function embedTrackingCode() {
-		var u = 'https://piwik.wikimedia.org/',
-			d = document,
-			g = d.createElement( 'script' ),
-			s = d.getElementsByTagName( 'head' )[ 0 ];
-		_paq.push( [ 'setTrackerUrl', u + 'piwik.php' ] );
-		_paq.push( [ 'setSiteId', '16' ] );
-		g.src = u + 'piwik.js';
-		g.async = true;
-		g.defer = true;
-		s.appendChild( g );
-	}
+/* eslint-disable-next-line no-implicit-globals */
+function embedTrackingCode() {
+	var u = 'https://piwik.wikimedia.org/',
+		d = document,
+		g = d.createElement( 'script' ),
+		s = d.getElementsByTagName( 'head' )[ 0 ];
+	_paq.push( [ 'setTrackerUrl', u + 'piwik.php' ] );
+	_paq.push( [ 'setSiteId', '16' ] );
+	g.src = u + 'piwik.js';
+	g.async = true;
+	g.defer = true;
+	s.appendChild( g );
+}
 
-	if ( window.addEventListener ) {
-		window.addEventListener( 'load', embedTrackingCode, false );
-	} else if ( window.attachEvent ) { /* Support for IE8 */
-		window.attachEvent( 'onload', embedTrackingCode );
-	} else {
-		embedTrackingCode();
-	}
-}() );
+if ( window.addEventListener ) {
+	window.addEventListener( 'load', embedTrackingCode, false );
+} else if ( window.attachEvent ) { /* Support for IE8 */
+	window.attachEvent( 'onload', embedTrackingCode );
+} else {
+	embedTrackingCode();
+}
